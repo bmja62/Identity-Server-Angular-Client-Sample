7f2c32bb3bd4fb1e4e59d5b7919a815aa9b9d64f
Bashir Momen <bashir.momen@amcsgroup.com>
2020-01-30 13:58:06 +0100

    call for open api

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html',
   styleUrls: ['./fetch-data.component.less']
 })
-export class FetchDataComponent implements OnInit {
+export class FetchDataComponent{
 
-  constructor() { }
+  public forecasts: WeatherForecast[];
 
-  ngOnInit() {
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('https://localhost:44316/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
   }
 
+}
+
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
